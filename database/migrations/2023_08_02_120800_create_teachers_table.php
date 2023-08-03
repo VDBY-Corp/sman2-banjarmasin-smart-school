279bdfcf914ba47e7621284c0b512997163b0ab5
Bayu Swari <bayuswari.01@gmail.com>
2023-08-03 14:13:02 +0000

    update: change uscolumn ername to email in teacher table

--- database/migrations/2023_08_02_120800_create_teachers_table.php
+++ database/migrations/2023_08_02_120800_create_teachers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 50);
             $table->string('gender', 15);
-            $table->string('username', 50);
+            $table->string('email', 30);
             $table->string('password', 20);
             $table->timestamps();
         });
