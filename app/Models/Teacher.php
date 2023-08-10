6554e5eea937f740117ea818f0b8bac43c350aee
Bayu Swari <bayuswari.01@gmail.com>
2023-08-10 17:18:09 +0000

    update: model teacher

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Teacher extends Model
+class Teacher extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = 'teachers';
     protected $primaryKey = 'id';
     public $incrementing = false;
+    protected $fillable = [
+        'name',
+        'gender',
+        'email',
+        'password'
+    ];
+    protected $hidden = ['password', 'remember_token'];
 }
