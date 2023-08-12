be6f8c9f8ba6e28a411ed4776c0b8c67289fa45e
Bayu Swari <bayuswari.01@gmail.com>
2023-08-12 17:44:53 +0000

    update: check role in file AuthenticatedSessionController

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -25,11 +25,23 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        // check role
+        if ($request->request->get("role") == "admin") {
+            $request->authenticate();
 
-        $request->session()->regenerate();
+            $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+            return redirect()->intended(RouteServiceProvider::HOME);
+        } else {
+            $request->TeacherAuthenticate();
+
+            $request->session()->regenerate();
+
+            return redirect()->intended(RouteServiceProvider::TEACHER);
+        }
+
+
+
     }
 
     /**
