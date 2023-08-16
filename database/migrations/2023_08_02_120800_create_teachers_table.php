4ea871055a2dccfcbfa9545a5d7fd2dce703a820
Bayu Swari <bayuswari.01@gmail.com>
2023-08-16 05:07:31 +0000

    update: change length email in teacher table

--- database/migrations/2023_08_02_120800_create_teachers_table.php
+++ database/migrations/2023_08_02_120800_create_teachers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 50);
             $table->string('gender', 15);
-            $table->string('email', 30);
+            $table->string('email', 100);
             $table->string('password', 255);
             $table->rememberToken();
             $table->timestamps();
