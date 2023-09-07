8d1c032fcbb2528752d105f2f3175afda606c3e3
Bayu Swari <bayuswari.01@gmail.com>
2023-09-07 23:09:07 +0700

    update: add image logo

--- resources/views/layouts/app-dashboard.blade.php
+++ resources/views/layouts/app-dashboard.blade.php
@@ -310,7 +310,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{ getDashboardGuardByCurrentRoute() }}" class="brand-link text-center">
-                {{-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
+                <img src="{{url('logo-sman-2-banjarmasin.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">{{ Setting::get('school.name') }}</span>
             </a>
 
